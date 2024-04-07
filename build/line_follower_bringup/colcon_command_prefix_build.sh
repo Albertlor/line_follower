507f9d46023c5f86a3eb7b2b87234f172bf0c36d
Albert <wensinlor@gmail.com>
2024-04-07 23:13:10 +0800

    added gazebo
    Signed-off-by: Albert <wensinlor@gmail.com>

---
+++ build/line_follower_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/albert/Workspaces/line_follower/install/line_follower_description/share/line_follower_description/package.sh"
